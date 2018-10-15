b32018c1a9f0ae96a025655c41272c26ca19ce1d
Eze Sunday Eze <mailsforeze@gmail.com>
2018-10-15 01:31:31 +0100

    updated toggle button

--- static/index.js
+++ static/index.js
@@ -7,10 +7,11 @@ sideMenuToggle.addEventListener("click", function() {
   if (sideMenu.style.display === "block") {
     sideMenu.style.display = "none";
     toggleWrapper.style.backgroundColor = "#48c6ef";
-    document.toggle.src = "../static/sidebar_toggler_icon_darkblue.png";
+    document.toggle.src = "static/sidebar_toggler_icon_darkblue.png";
   } else {
     sideMenu.style.display = "block";
     toggleWrapper.style.backgroundColor = "#c51162";
     toggleWrapper.style.color = "white";
+    document.toggle.src = "static/sidebar_toggler_icon_darkblue.png";
   }
 });
